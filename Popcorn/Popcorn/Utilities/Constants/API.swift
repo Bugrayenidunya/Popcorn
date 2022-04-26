fcc4f347f382e62a2473921534190d643f0d34a6
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-04-26 18:04:16 +0300

    Add constants

---
+++ Popcorn/Popcorn/Utilities/Constants/API.swift
@@ -0,0 +1,17 @@
+//
+//  API.swift
+//  Popcorn
+//
+//  Created by Enes Buğra Yenidünya on 26.04.2022.
+//
+
+import Foundation
+
+extension Constant {
+    
+    /// Constant `ÀPI` variables
+    class API {
+        static let baseUrl = "jsonplaceholder.typicode.com"
+    }
+    
+}
---
+++ Popcorn/Popcorn/Utilities/Constants/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  Popcorn
+//
+//  Created by Enes Buğra Yenidünya on 26.04.2022.
+//
+
+import Foundation
+
+
+/// Base class for handling `Constant` variables at whole project
+class Constant { }
