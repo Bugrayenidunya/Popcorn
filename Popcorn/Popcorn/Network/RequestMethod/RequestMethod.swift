16de5ad6981160cc5c65759831185a77ab0f70d4
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-04-26 18:05:00 +0300

    Add request method enum

---
+++ Popcorn/Popcorn/Network/RequestMethod/RequestMethod.swift
@@ -0,0 +1,14 @@
+//
+//  RequestMethod.swift
+//  Popcorn
+//
+//  Created by Enes Buğra Yenidünya on 26.04.2022.
+//
+
+import Foundation
+
+/// Use this enum to determine your request method when making a network call
+enum RequestMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
