df86f9d88ae20629a275990b2c18de075186a778
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-04-26 21:25:50 +0300

    Add constant for user defaults

---
+++ Popcorn/Popcorn/Utilities/Constants/UserDefautls.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefautls.swift
+//  Popcorn
+//
+//  Created by Enes Buğra Yenidünya on 26.04.2022.
+//
+
+import Foundation
+
+extension Constant {
+
+    /// Constant `UserDefaults` variables
+    class UserDefaults {
+        
+    }
+
+}
